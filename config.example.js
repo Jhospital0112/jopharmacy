a89f54e43826b82fc309358a34c7647ec058089a
Jhospital0112 <nooraburyaleh02@gmail.com>
2026-03-26 19:29:27 +0300

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,3 @@
+window.CDMS_CONFIG = {
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXX/exec'
+};
---
+++ config.js
@@ -0,0 +1,3 @@
+window.CDMS_CONFIG = {
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxGjc1Ifxt30NG32rpt_Uc9lg7Z04t8BKBGl1ksHSrqCXTYdyc6XY-9XzdfsM09FsQ4Xw/exec'
+};
