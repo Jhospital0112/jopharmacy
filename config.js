29e718ab7ed6a579dc7de9a8a01029ea9b713037
Jhospital0112 <nooraburyaleh02@gmail.com>
2026-03-27 17:48:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CDMS_CONFIG = {
-  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx8Uz6JVhthGmvGTWytasZjA8E9l3z7poe1d4n4lm7kJ4PaSHDCTV1JdVUaM-EB-sqLsg/exec'
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbwpmNQraqZSuZOGu2xhHcC5gt5Dlv99JtXIMSvj7Fy_wFeELscgowN5XPDRYImH0I8F/exec'
 };
